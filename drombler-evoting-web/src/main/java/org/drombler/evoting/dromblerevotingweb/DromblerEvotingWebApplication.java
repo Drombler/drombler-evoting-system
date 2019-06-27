eda54c4a1a86a45f8c8e17400bfc2a078c3c3d6b
Florian Brunner <fbrunnerreg@gmx.ch>
2019-06-27 23:21:49 +0200

    initial import

---
+++ drombler-evoting-web/src/main/java/org/drombler/evoting/dromblerevotingweb/DromblerEvotingWebApplication.java
@@ -0,0 +1,13 @@
+package org.drombler.evoting.dromblerevotingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DromblerEvotingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DromblerEvotingWebApplication.class, args);
+	}
+
+}
---
+++ drombler-evoting-web/src/test/java/org/drombler/evoting/dromblerevotingweb/DromblerEvotingWebApplicationTests.java
@@ -0,0 +1,16 @@
+package org.drombler.evoting.dromblerevotingweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DromblerEvotingWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
